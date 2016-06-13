15678eb80b46e3fd13d9e064e90b9e8a1bfd11bf
fanjun <fanjun@shinhofood.com>
2016-06-13 18:01:12 +0800

    add GSon as a solution for rest result

--- src/main/java/cn/fanstar/server/controller/DemoController.java
+++ src/main/java/cn/fanstar/server/controller/DemoController.java
@@ -3,6 +3,7 @@ package cn.fanstar.server.controller;
 import cn.fanstar.server.common.ResponseGSon;
 import cn.fanstar.server.dto.Account;
 import cn.fanstar.server.service.AccountService;
+import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +21,13 @@ public class DemoController {
     private AccountService accountService;
 
     @RequestMapping(value="/demo", method=RequestMethod.GET)
-    public ResponseGSon getInfo(){
+    public String getInfo(){
+        logger.info("Request:/demo");
         Map<String,String> result = new HashMap<>();
         result.put("message","greeting from Frank ");
-        return ResponseGSon.buildSuccessResult(result);
+        String resGson = new Gson().toJson(result);
+        logger.info(String.format("Request:/demo result: %s", resGson));
+        return resGson;
     }
 
     @RequestMapping(value="/get-account", method = RequestMethod.GET)
