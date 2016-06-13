cd426a7a2d7856439a3879ea0b829bf2fdb4b59f
fanjun <fanjun@shinhofood.com>
2016-06-13 14:34:32 +0800

    add hash equal toString builder by using apache common resource

--- src/main/java/cn/fanstar/server/dto/Account.java
+++ src/main/java/cn/fanstar/server/dto/Account.java
@@ -1,6 +1,10 @@
 package cn.fanstar.server.dto;
 
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import java.io.Serializable;
 
 public class Account implements Serializable {
@@ -170,4 +174,18 @@ public class Account implements Serializable {
     this.bannerName = bannerName;
   }
 
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
 }
